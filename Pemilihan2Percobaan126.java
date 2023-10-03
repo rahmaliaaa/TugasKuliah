3c5713f11f348dbf8cf7291adaa7f09c276b82ef
rahmaliaaa <mozachikaaa@gmail.com>
2023-10-03 10:46:10 +0700

    jobsheet6

--- Pemilihan2Percobaan126.java
+++ Pemilihan2Percobaan126.java
@@ -8,6 +8,8 @@ public class Pemilihan2Percobaan126 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100 != 0)) {
                 System.out.println("Tahun Kabisat");
+            }else if ((tahun % 400) == 0) {
+                System.out.println("Tahun kabisat");
             }else 
             System.out.println("Bukan Tahun Kabisat");
 
