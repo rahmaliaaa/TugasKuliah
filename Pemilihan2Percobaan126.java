24655a5a371d26a4a9b426fb8ca02eee1bb62715
rahmaliaaa <mozachikaaa@gmail.com>
2023-10-03 10:38:11 +0700

    jobsheet6

--- Pemilihan2Percobaan126.java
+++ Pemilihan2Percobaan126.java
@@ -6,10 +6,15 @@ public class Pemilihan2Percobaan126 {
         int tahun = input26.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100 != 0)) {
                 System.out.println("Tahun Kabisat");
+            }else 
+            System.out.println("Bukan Tahun Kabisat");
+
         } else 
             System.out.println("Bukan Tahun Kabisat");
+
     }
-    
 }
+
+
