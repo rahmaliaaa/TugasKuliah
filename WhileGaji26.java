698de7dcc48031eaae437199a5f0a85c21002f49
rahmaliaaa <mozachikaaa@gmail.com>
2023-10-10 12:14:29 +0700

    commit

--- WhileGaji26.java
+++ WhileGaji26.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 public class WhileGaji26 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int jumlahKaryawan, jumlahJamLembur;
         double gajiLembur = 0, totalGajiLembur = 0;
         String jabatan;
         System.out.print("Masukkan jumlah karyawan: ");
-        jumlahKaryawan = input.nextInt();
+        jumlahKaryawan = sc.nextInt();
         int i = 0;
 
         while (i < jumlahKaryawan) {
             System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
-            jabatan = input.next();
+            jabatan = sc.next();
             System.out.print("Masukkan jumlah jam lembur: ");
-            jumlahJamLembur = input.nextInt();
+            jumlahJamLembur = sc.nextInt();
             i++;
 
             if (jabatan.equalsIgnoreCase("direktur")) {
