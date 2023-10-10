142863477b7b85fe9e344b13adf1584171ca3a68
rahmaliaaa <mozachikaaa@gmail.com>
2023-10-10 09:45:33 +0700

    commit

--- ForKelipatan26.java
+++ ForKelipatan26.java
@@ -14,7 +14,10 @@ public class ForKelipatan26 {
                 counter++;
             }
         }
+        double rataRata = (double) total / counter;
+
 System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
 System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalalah %.2f\n", kelipatan, rataRata);
     }
 }
