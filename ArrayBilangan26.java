81297380069b6b79e5b918071df010f84155f942
rahmaliaaa <mozachikaaa@gmail.com>
2023-10-24 09:46:16 +0700

    commit

--- ArrayBilangan26.java
+++ ArrayBilangan26.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
 public class ArrayBilangan26 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int [] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        double[] bil = new double[4];
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+            
+            bil[0] = 5.0;
+            bil[1] = 12867;
+            bil[2] = 7.5;
+            bil[3] = 2000000;
+    
+            System.out.println(bil [0]);
+            System.out.println(bil [1]);
+            System.out.println(bil [2]);
+            System.out.println(bil [3]);
+        }
     }
 }
