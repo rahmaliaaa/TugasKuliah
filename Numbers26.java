d158ccdb9ac1c52f76b2b3b025fa191a39157ff5
rahmaliaaa <mozachikaaa@gmail.com>
2023-11-01 00:39:17 +0700

    update

--- Numbers26.java
+++ Numbers26.java
@@ -7,7 +7,7 @@ public class Numbers26 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
-        for (int i = 0; i < Numbers26.length; i++) {
+        for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
     }
