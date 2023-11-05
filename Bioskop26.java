f1ff0fb2bed06691c60f7fe2fd1018a120868343
rahmaliaaa <mozachikaaa@gmail.com>
2023-11-05 14:48:23 +0700

    modifikasi nomor 10

--- Bioskop26.java
+++ Bioskop26.java
@@ -16,11 +16,11 @@ public class Bioskop26 {
     System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
     System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-    //tambahan
-    System.out.println(penonton.length);
+    //modifikasi nomor 10
+    System.out.println("Penonton pada baris ke-3: ");
 
-    for (String[] barisPenonton : penonton) {
-      System.out.println("Panjang baris ke : " +barisPenonton.length);
+    for (int i = 0; i < penonton[2].length; i++) {
+      System.out.println(penonton[2][i]);
     }
 
   }  
